e457dd17fd0186d33d09758b0e15c7aa3b7ce748
David <davrv93@gmail.com>
2018-11-11 19:03:43 -0500

    portugues activado

--- www/apps/reader/controller/spirit_prophecy_read.js
+++ www/apps/reader/controller/spirit_prophecy_read.js
@@ -1,75 +1,105 @@
-
 angular.module('starter').controller('SpiritProphecyReadCtrl', function ($scope, $http, $filter, API_READER, $stateParams, $translate, $rootScope) {
 
-	var $translateFilter = $filter('translate');
+  var $translateFilter = $filter('translate');
 
-	$rootScope.change_language = function (locale) {
-		$translate.use(locale);
-		localStorage.language = locale;
-		$scope.onListReading();
-
-	}
-
-
-	$scope.HighLight = function () {
-		var elementId = event.srcElement.id;
-		var elementHtml = document.getElementById(elementId)
-		var BackgroundColorHighlight = "#337BDF";
-		var BackgroundColor = "#93B499";
-		var HexBackgroundColor = "rgb(51, 123, 223)";
-
-		if (elementHtml.style.backgroundColor === HexBackgroundColor) {
-			elementHtml.style.backgroundColor = BackgroundColor;
-		}
-		else {
-			elementHtml.style.backgroundColor = BackgroundColorHighlight;
-		}
-	}
+  $rootScope.change_language = function (locale) {
+    $translate.use(locale);
+    localStorage.language = locale;
+    $rootScope.progress = true;
+    var currentDate = new Date()
+    var day = currentDate.getDate();
+    var month = currentDate.getMonth() + 1;
+    var year = currentDate.getFullYear();
 
 
+    if (day < 10) {
+      day = '0' + day
+    }
 
-	$scope.onListReading = function () {
-		$rootScope.progress = true;
-		var currentDate = new Date()
-		var day = currentDate.getDate();
-		var month = currentDate.getMonth() + 1;
-		var year = currentDate.getFullYear();
-		var code_iso = localStorage.language;
+    if (month < 10) {
+      month = '0' + month
+    }
 
-
-		if (day < 10) {
-			day = '0' + day
-		}
-
-		if (month < 10) {
-			month = '0' + month
-		}
-
-		var param_date = year + '-' + month + '-' + day;
+    var param_date = year + '-' + month + '-' + day;
 
 
-		var data = {
-			start_date: param_date,
-			end_date: param_date,
-			code_iso: code_iso
-		}
+    var data = {
+      start_date: param_date,
+      end_date: param_date,
+      code_iso: locale
+    }
 
-		console.log(data)
+    console.log(data)
 
 
 
-		API_READER.SpiritProphecy.get(data).$promise.then(function (success) {
-			$scope.reading = success[0];
+    API_READER.SpiritProphecy.get(data).$promise.then(function (success) {
+      $scope.reading = success[0];
 
-		});
+    });
 
 
-	}
 
-	console.log('test')
+  }
 
-	$scope.onListReading();
+
+  $scope.HighLight = function () {
+    var elementId = event.srcElement.id;
+    var elementHtml = document.getElementById(elementId)
+    var BackgroundColorHighlight = "#337BDF";
+    var BackgroundColor = "#93B499";
+    var HexBackgroundColor = "rgb(51, 123, 223)";
+
+    if (elementHtml.style.backgroundColor === HexBackgroundColor) {
+      elementHtml.style.backgroundColor = BackgroundColor;
+    } else {
+      elementHtml.style.backgroundColor = BackgroundColorHighlight;
+    }
+  }
+
+
+
+  $scope.onListReading = function () {
+    $rootScope.progress = true;
+    var currentDate = new Date()
+    var day = currentDate.getDate();
+    var month = currentDate.getMonth() + 1;
+    var year = currentDate.getFullYear();
+    var code_iso = localStorage.language;
+
+
+    if (day < 10) {
+      day = '0' + day
+    }
+
+    if (month < 10) {
+      month = '0' + month
+    }
+
+    var param_date = year + '-' + month + '-' + day;
+
+
+    var data = {
+      start_date: param_date,
+      end_date: param_date,
+      code_iso: $translate.use()
+    }
+
+    console.log(data)
+
+
+
+    API_READER.SpiritProphecy.get(data).$promise.then(function (success) {
+      $scope.reading = success[0];
+
+    });
+
+
+  }
+
+  console.log('test')
+
+  $scope.onListReading();
 
 
 })
-
